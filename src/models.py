fc0a6cd005c213bd38f6ce91d40c61ee1e85659e
Margoth <mloreana@gmail.com>
2021-04-16 01:29:50 +0000

    APISW1

--- src/models.py
+++ src/models.py
@@ -8,6 +8,23 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+
+class Planets(db.Model):
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    population = db.Column(db.Integer)
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+    climate = db.Column(db.String(40))
+    gravity = db.Column(db.String(40))
+    terrain = db.Column(db.String(40))
+    surface_water = db.Column(db.Integer)
+    gravity = db.Column(db.String(40))
+    
+
     def __repr__(self):
         return '<User %r>' % self.username
 
